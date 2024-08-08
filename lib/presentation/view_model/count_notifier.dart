53f4beb8b2b9939a89630a6a9ed93e367527873c
taimatu7 <taimatu7@outlook.jp>
2024-08-08 19:21:58 +0900

    add コメント追加

--- lib/presentation/view_model/count_notifier.dart
+++ lib/presentation/view_model/count_notifier.dart
@@ -2,13 +2,19 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../model/count.dart';
 
+// 生成されるファイル名: count_notifier.riverpod.dart
 part 'count_notifier.g.dart';
 
+// @riverpodのアノテーションを付与
+// countNotifierProviderというグローバル変数が自動で宣言される(生成ファイル内にあり)
 @riverpod
 class CountNotifier extends _$CountNotifier {
+  // 保持する値の型を戻り値に指定
+  // buildは初期値を返すようにする
   @override
   Count build() => Count(0);
 
+  // 状態を更新する
   void increment() {
     state = state.increment();
   }
